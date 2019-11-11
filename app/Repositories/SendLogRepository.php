bbd39f39ffdd40c9d4aa3a982d30807e93800d85
Igor Tsurikov <itsurikovcom@gmail.com>
2019-11-11 23:29:14 +0200

    have created interfaces and repositories: UserRepository, CountryRepository, NumberRepository, SendLogRepository

---
+++ app/Repositories/SendLogRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: itens
+ * Date: 11.11.19
+ * Time: 23:14
+ */
+
+namespace App\Repositories;
+
+
+class SendLogRepository
+{
+
+}
---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: itens
+ * Date: 11.11.19
+ * Time: 23:13
+ */
+
+namespace App\Repositories;
+
+
+class UserRepository
+{
+
+}
