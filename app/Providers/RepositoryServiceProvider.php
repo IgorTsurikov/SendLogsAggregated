2aecb041431acdc554b6368720fe062731335d4c
Igor Tsurikov <itsurikovcom@gmail.com>
2019-11-12 00:06:45 +0200

    have created interface and repository SendLogAggregated

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -5,8 +5,10 @@ namespace App\Providers;
 use App\Repositories\CountryRepository;
 use App\Repositories\Interfaces\CountryRepositoryInterface;
 use App\Repositories\Interfaces\NumberRepositoryInterface;
+use App\Repositories\Interfaces\SendLogAggregatedRepositoryInterface;
 use App\Repositories\Interfaces\SendLogRepositoryInterface;
 use App\Repositories\NumberRepository;
+use App\Repositories\SendLogAggregatedRepository;
 use App\Repositories\SendLogRepository;
 use App\Repositories\UserRepository;
 use App\Repositories\UserRepositoryInterface;
@@ -35,5 +37,6 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(CountryRepositoryInterface::class, CountryRepository::class);
         $this->app->bind(NumberRepositoryInterface::class, NumberRepository::class);
         $this->app->bind(SendLogRepositoryInterface::class, SendLogRepository::class);
+        $this->app->bind(SendLogAggregatedRepositoryInterface::class, SendLogAggregatedRepository::class);
     }
 }
---
+++ app/Repositories/SendLogAggregatedRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: itens
+ * Date: 12.11.19
+ * Time: 0:03
+ */
+
+namespace App\Repositories;
+
+
+class SendLogAggregatedRepository
+{
+
+}
