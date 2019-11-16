4d1bca547ae56d58c193ee7215ae415c33884d0b
Igor Tsurikov <itsurikovcom@gmail.com>
2019-11-16 21:57:51 +0200

    created base layout

--- app/Http/Controllers/SendLogAggregatedController.php
+++ app/Http/Controllers/SendLogAggregatedController.php
@@ -8,6 +8,6 @@ class SendLogAggregatedController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        return view('sendLogAggregated');
     }
 }
---
+++ resources/views/sendLogAggregated.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Send Log Aggregated</title>
+    </head>
+    <body>
+        <div>
+            <div>
+                <h1>Send Log Aggregated</h1>
+            </div>
+            <div>
+                <div>
+                    Country:
+                </div>
+                <div>
+                    User:
+                </div>
+                <div>
+                    From:
+                </div>
+                <div>
+                    To:
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
